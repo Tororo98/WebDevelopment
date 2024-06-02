d7a2de9ed691b49554b1b768374a8affed91ff38
Tororo98 <sebastiantofra98@javerianacali.edu.co>
2024-06-02 15:49:22 -0500

    Use of DeleteView to delete existing entity

--- src/webProject/base/urls.py
+++ src/webProject/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import ListPendings, DetalleTarea, CrearTarea, EditarTarea
+from .views import ListPendings, DetalleTarea, CrearTarea, EditarTarea, EliminarTarea
 
 urlpatterns = [path('', ListPendings.as_view(), name='pendings'),
                path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea'),
                path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
-               path('editar-tarea/<int:pk>', EditarTarea.as_view(), name='editar-tarea')]
+               path('editar-tarea/<int:pk>', EditarTarea.as_view(), name='editar-tarea'),
+               path('eliminar-tarea/<int:pk>', EliminarTarea.as_view(), name='eliminar-tarea')]
--- src/webProject/base/views.py
+++ src/webProject/base/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Tarea
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.views.generic.edit import UpdateView
 
 
 # Create your views here.
@@ -32,3 +31,9 @@ class EditarTarea(UpdateView):
     model = Tarea
     fields = '__all__'
     success_url = reverse_lazy('pendings')
+
+class EliminarTarea(DeleteView):
+    model = Tarea
+    context_object_name = 'tarea'
+    success_url = reverse_lazy('pendings')
+    template_name = 'base/tarea_confirm_delete.html'
